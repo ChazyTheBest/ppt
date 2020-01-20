3cc28f0b104efac4a276d4d88c3fea32edb43b17
Gabriel Blanca Montes <how2hack@hotmail.es>
2020-01-21 00:06:03 +0100

    Implementadas dos nuevas opciones: lagartija y spock

--- PiedraPapelTijera/src/MoveWords.java
+++ PiedraPapelTijera/src/MoveWords.java
@@ -6,27 +6,27 @@ public class MoveWords{
     public static final int GANA = 1;
     public static final int PIERDE = 2;
 
-    private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA"};
+    private static final String[] validMoves = {"LAGARTIJA", "PAPEL", "PIEDRA", "SPOCK", "TIJERAS"};
     private static final String[] validCommands = {"SALIR", "HELP"};
 
     private Random rnd;
-    
+
     public MoveWords(){
     	rnd = new Random();
     }
-    
+
     public boolean isValidMoveCommand(String value){
 
 	    for (int i =0; i< validMoves.length; i++){
 	        if(validMoves[i].equals(value))
 	    	    return true;
 	    }
-	    
+
 	    for (int i =0; i< validCommands.length; i++){
 	        if(validCommands[i].equals(value))
 		        return true;
 	    }
-	    
+
 	    return false;
     }
 
@@ -48,7 +48,7 @@ public class MoveWords{
 
     public void showWords(){
         showMoves();
-        showCommands();    
+        showCommands();
     }
 
     private static int getIndex(String value){
@@ -61,13 +61,44 @@ public class MoveWords{
 
     public static int checkWinner(String first, String second){
 	    int first_i, second_i;
+	    boolean win;
 
 	    first_i = getIndex(first);
 	    second_i = getIndex(second);
 
 	    if (first_i == second_i) return EMPATE;
-	    
-	    return (( (first_i +1) % validMoves.length ) == second_i ) ? GANA: PIERDE;
+
+	    switch (first_i)
+	    {
+            case 0:
+            {
+                win = second_i == 1 || second_i == 3 ? true : false;
+                break;
+            }
+            case 1:
+            {
+                win = second_i == 2 || second_i == 3 ? true : false;
+                break;
+            }
+            case 2:
+            {
+                win = second_i == 0 || second_i == 4 ? true : false;
+                break;
+            }
+            case 3:
+            {
+                win = second_i == 2 || second_i == 4 ? true : false;
+                break;
+            }
+            case 4:
+            {
+                win = second_i == 0 || second_i == 1 ? true : false;
+                break;
+            }
+            default: win = false;
+	    }
+
+	    return win ? GANA : PIERDE;
 	}
-	
-} 
+
+}
